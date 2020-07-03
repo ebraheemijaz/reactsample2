ecabbb456d53d41dc436bd3bb34a3aabfbdbc6b4
Ebraheem <ebraheemijaz913@gmail.com>
2020-07-03 11:51:41 +0500

    Updates

---
+++ precache-manifest.72ce44fab61222cb8786bb5847be03ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce94fb551bd1b6335fd0317f4bd2a87",
+    "url": "/reactsample2/index.html"
+  },
+  {
+    "revision": "d159a18c92ebf8783d0d",
+    "url": "/reactsample2/static/css/main.a70896a2.chunk.css"
+  },
+  {
+    "revision": "6493459baf36beaf106a",
+    "url": "/reactsample2/static/js/2.f2472e02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactsample2/static/js/2.f2472e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d159a18c92ebf8783d0d",
+    "url": "/reactsample2/static/js/main.a99a89aa.chunk.js"
+  },
+  {
+    "revision": "ba57c87353b7daa8cc3f",
+    "url": "/reactsample2/static/js/runtime-main.c6c05e95.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/reactsample2/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/reactsample2/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/reactsample2/static/media/spinner.235b9b3e.gif"
+  }
+]);
